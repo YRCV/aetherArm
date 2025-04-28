b4b750ad791db1b287a40a30b73cd96eda20bf7e
MosesEspejo <mosesespejo@gmail.com>
2025-04-28 15:50:36 -0400

    Testing

--- src/main.cpp
+++ src/main.cpp
@@ -8,8 +8,9 @@ void setup() {
 
 void loop() {
   Serial.println("Xiao is running ");
-  digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
-  delay(1000);                      // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
+  digitalWrite(LED_BUILTIN, HIGH); 
+  delay(1000);                    
+  digitalWrite(LED_BUILTIN, LOW);
   delay(1000);
+  //testing
 }
